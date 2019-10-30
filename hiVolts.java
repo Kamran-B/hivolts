d5af89c4d77d25fcd3d212073aab50ec654fc9c0
Kamran <56656396+Kamran-B@users.noreply.github.com>
2019-10-29 21:22:00 -0700

    Update hiVolts.java

--- hiVolts.java
+++ hiVolts.java
@@ -11,23 +11,30 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Random;
 import javax.swing.JFrame;
 
-public class hiVolts extends JFrame implements KeyListener {
+public class hiVolts extends JFrame implements KeyListener, MouseListener {
+
 	Random rand = new Random();
 	int c;
 	int r;
 	int inputKey;
 	boolean dead;
+	boolean notPlay = false;
 	int var;
 	int first;
-	
-	//Sets the size of the board to 12 x 12
+	boolean pTurn = true;
+
 	int[][] boardPos = new int[12][12];
 	int[] counterPos = new int[2];
+	int[][] mhoPos = new int[12][2];
+	int[][] fencePos = new int[20][2];
 
-	// This draws the board 
+
+	// This draws the board
 	public void drawboard(Graphics g) {
 		for(int col = 0; col<12; col++) {
 			for(int row = 0; row<12; row++) {
@@ -38,36 +45,38 @@ public class hiVolts extends JFrame implements KeyListener {
 		}
 		for(int i = 0; i<20; i++) {
 			while(true) {
-				c = rand.nextInt(9);
+				c = rand.nextInt(10);
 				c += 2;
-				r = rand.nextInt(9);
+				r = rand.nextInt(10);
 				r += 2;
 				if(boardPos[c-1][r-1]==0) {
 					boardPos[c-1][r-1] = 1;
 					break;
 				}
 			}
-			counterPos[0] = r;
-			counterPos[1] = c;
+			fencePos[i][0] = c;
+			fencePos[i][1] = r;
 			fence(g, calcCoord(c), calcCoord(r));
 		}
 		for(int i = 0; i<12; i++) {
 			while(true) {
-				c = rand.nextInt(9);
-				c += 3;
-				r = rand.nextInt(9);
+				c = rand.nextInt(10);
+				c += 2;
+				r = rand.nextInt(10);
 				r += 2;
 				if(boardPos[c-1][r-1]==0) {
 					boardPos[c-1][r-1] = 2;
 					break;
 				}
 			}
+			mhoPos[i][0] = c;
+			mhoPos[i][1] = r;
 			mho(g, calcCoord(c), calcCoord(r));
 		}
 		while(true) {
-			c = rand.nextInt(9);
+			c = rand.nextInt(10);
 			c += 2;
-			r = rand.nextInt(9);
+			r = rand.nextInt(10);
 			r += 2;
 			if(boardPos[c-1][r-1]==0) {
 				boardPos[c-1][r-1] = 3;
@@ -85,24 +94,20 @@ public class hiVolts extends JFrame implements KeyListener {
 		}
 	}
 
-	// This figures out whether or not the player is dead at a given position
-	public boolean isdeadPlayer(int newPosX, int newPosY) {
+	// This figures out whether or not the player (or mho) is dead at a given position
+	public boolean isDeadPlayer(int newPosX, int newPosY) {
 		dead = false;
-		for(int col = 0; col<12; col++) {
-			for(int row = 0; row<12; row++) {
-				if((newPosX==1||newPosX==2)&&(newPosY==1||newPosY==2)) {
-					dead = true;
-				}
-			}
+		if((boardPos[newPosX-1][newPosY-1]==1)||(boardPos[newPosX-1][newPosY-1]==2)) {
+			dead = true;
 		}
 		return dead;
 	}
-	
-	public boolean isdeadMho(int newPosX, int newPosY) {
+
+	public boolean isDeadMho(int newPosX, int newPosY) {
 		dead = false;
 		for(int col = 0; col<12; col++) {
 			for(int row = 0; row<12; row++) {
-				if((newPosX==1&&newPosY==1)) {
+				if((boardPos[newPosX-1][newPosY-1]==1)) {
 					dead = true;
 				}
 			}
@@ -112,8 +117,6 @@ public class hiVolts extends JFrame implements KeyListener {
 
 	// This method draws the electric fence
 	public void fence(Graphics g, int startX, int startY) {
-		int nextX = startX;
-		int nextY = startY;
 		g.drawLine(startX, startY, startX, startY+20);
 
 		g.drawLine(startX, startY+5, startX+20, startY+5);
@@ -121,11 +124,6 @@ public class hiVolts extends JFrame implements KeyListener {
 		g.drawLine(startX, startY+15, startX+20, startY+15);
 
 		g.drawLine(startX+20, startY, startX+20, startY+20);
-	}
-
-	// This calculates the graphics coordinate based on the grid place
-	public int calcCoord(int pos) {
-		return 50+(pos-1)*30;
 	}
 
 	// Draws a mho
@@ -148,57 +146,263 @@ public class hiVolts extends JFrame implements KeyListener {
 		g.setColor(Color.YELLOW);
 	}
 
+	// This calculates the graphics coordinate based on the grid place
+	public int calcCoord(int pos) {
+		return 50+(pos-1)*30;
+	}
+
 	// NOT FINISHED - intended to move a mho
 	public void moveMho(Graphics g) {
 		for(int mho = 0; mho<12; mho++) {
-			int col = mhoPos[mho][0];
-			int row = mhoPos[mho][1];
-			if(col==counterPos[0]) {
-				if(row<counterPos[1]) {
-					boardPos[col][row] = 0;
-					if(isdeadMho(col, row+1)==true) {
-						g.setColor(Color.BLACK);
-						g.fillRect(calcCoord(col), calcCoord(row), 20, 20);
-						g.setColor(Color.YELLOW);
+			if(mhoPos[mho][0]!=13) {
+				int col = mhoPos[mho][0];
+				int row = mhoPos[mho][1];
+				System.out.println("1");
+				if(col==counterPos[0]) {
+					System.out.println("2");
+					if(row<counterPos[1]) {
+						System.out.println("3");
 						boardPos[col][row] = 0;
+						if(isDeadMho(col, row+1)==true) {
+							System.out.println("4");
+							g.setColor(Color.BLACK);
+							g.fillRect(calcCoord(col)-1, calcCoord(row)-1, 22, 22);
+							g.setColor(Color.YELLOW);
+							boardPos[col][row] = 0;
+							mhoPos[mho][0] = col;
+							mhoPos[mho][1] = row;
+						}
+						else {
+							System.out.println("5");
+							boardPos[col][row+1] = 2;
+							//if((counterPos[0]==col)&&(counterPos[0]==row)) {
+								g.setColor(Color.BLACK);
+								g.fillRect(calcCoord(col)-1, calcCoord(row)-1, 22, 22);
+								g.setColor(Color.YELLOW);
+								mho(g, calcCoord(col), calcCoord(row+1));
+								boardPos[col][row] = 0;
+								mhoPos[mho][0] = 13;
+							//}
+						}
 					}
-					else {
-						boardPos[col][row+1] = 2;
-						if((counterPos[0]==col)&&(counterPos[0]==row)) {
-							// Dead
+					else if(row>counterPos[1]) {
+						System.out.println("6");
+						boardPos[col][row] = 0;
+						if(isDeadMho(col, row-1)==true) {
+							System.out.println("7");
+							g.setColor(Color.BLACK);
+							g.fillRect(calcCoord(col)-1, calcCoord(row)-1, 22, 22);
+							g.setColor(Color.YELLOW);
+							boardPos[col][row] = 0;
+						}
+						else {
+							System.out.println("8");
+							boardPos[col][row-1] = 2;
+							//if((counterPos[0]==col)&&(counterPos[0]==row)) {
+								g.setColor(Color.BLACK);
+								g.fillRect(calcCoord(col)-1, calcCoord(row)-1, 22, 22);
+								mho(g, calcCoord(col), calcCoord(row-1));
+								boardPos[col][row] = 0;
+								mhoPos[mho][0] = 13;
+							//}
 						}
 					}
 				}
-				else if(row>counterPos[1]) {
-					boardPos[col][row] = 0;
-					if(isdeadMho(col, row-1)==true) {
-						g.setColor(Color.BLACK);
-						g.fillRect(calcCoord(col), calcCoord(row), 20, 20);
-						g.setColor(Color.YELLOW);
-						boardPos[col][row] = 0;
-					}
-					else {
-						boardPos[col][row-1] = 2;
-						if((counterPos[0]==col)&&(counterPos[0]==row)) {
-							// Dead
-						}
-					}
-				}
-			}
-			if(row==counterPos[1]) {
-				if(col<counterPos[0]) {
+				if(row==counterPos[1]) {
+					if(col<counterPos[0]) {
 
+					}
 				}
 			}
 		}
 	}
 
+	public void dead(Graphics g) {
+		testHiVolts.end = true;
+		g.drawString("GAME OVER", 500, 100);
+		g.drawString("Click here to play again:", 460, 120);
+	}
+
+	public void up(Graphics g) {
+		if(isDeadPlayer(counterPos[0], counterPos[1]-1)) {
+			dead(g);
+		}
+		else {
+			g.setColor(Color.BLACK);
+			g.fillRect(calcCoord(counterPos[0])-1, calcCoord(counterPos[1])-1, 22, 22);
+			g.setColor(Color.RED);
+			counter(g, calcCoord(counterPos[0]), calcCoord(counterPos[1]-1));
+			counterPos[1] -= 1;
+		}
+		moveMho(g);
+	}
+
+	public void down(Graphics g) {
+		if(isDeadPlayer(counterPos[0], counterPos[1]+1)) {
+			dead(g);
+		}
+		else {
+			g.setColor(Color.BLACK);
+			g.fillRect(calcCoord(counterPos[0])-1, calcCoord(counterPos[1])-1, 22, 22);
+			g.setColor(Color.RED);
+			counter(g, calcCoord(counterPos[0]), calcCoord(counterPos[1]+1));
+			counterPos[1] += 1;
+		}
+		moveMho(g);
+	}
+
+	public void right(Graphics g) {
+		if(isDeadPlayer(counterPos[0]+1, counterPos[1])) {
+			dead(g);
+		}
+		else {
+			g.setColor(Color.BLACK);
+			g.fillRect(calcCoord(counterPos[0])-1, calcCoord(counterPos[1])-1, 22, 22);
+			g.setColor(Color.RED);
+			counter(g, calcCoord(counterPos[0]+1), calcCoord(counterPos[1]));
+			counterPos[0] += 1;
+		}
+		moveMho(g);
+	}
+
+	public void left(Graphics g) {
+		if(isDeadPlayer(counterPos[0]-1, counterPos[1])) {
+			dead(g);
+		}
+		else {
+			g.setColor(Color.BLACK);
+			g.fillRect(calcCoord(counterPos[0])-1, calcCoord(counterPos[1])-1, 22, 22);
+			g.setColor(Color.RED);
+			counter(g, calcCoord(counterPos[0]-1), calcCoord(counterPos[1]));
+			counterPos[0] -= 1;
+		}
+		moveMho(g);
+	}
+
+	public void rightUp(Graphics g) {
+		if(isDeadPlayer(counterPos[0]+1, counterPos[1]-1)) {
+			dead(g);
+		}
+		else {
+			g.setColor(Color.BLACK);
+			g.fillRect(calcCoord(counterPos[0])-1, calcCoord(counterPos[1])-1, 22, 22);
+			g.setColor(Color.RED);
+			counter(g, calcCoord(counterPos[0]+1), calcCoord(counterPos[1]-1));
+			counterPos[0] += 1;
+			counterPos[1] -= 1;
+		}
+		moveMho(g);
+	}
+
+	public void leftUp(Graphics g) {
+		if(isDeadPlayer(counterPos[0]-1, counterPos[1]-1)) {
+			dead(g);
+		}
+		else {
+			g.setColor(Color.BLACK);
+			g.fillRect(calcCoord(counterPos[0])-1, calcCoord(counterPos[1])-1, 22, 22);
+			g.setColor(Color.RED);
+			counter(g, calcCoord(counterPos[0]-1), calcCoord(counterPos[1]-1));
+			counterPos[0] -= 1;
+			counterPos[1] -= 1;
+		}
+		moveMho(g);
+	}
+
+	public void leftDown(Graphics g) {
+		if(isDeadPlayer(counterPos[0]-1, counterPos[1]+1)) {
+			dead(g);
+		}
+		else {
+			g.setColor(Color.BLACK);
+			g.fillRect(calcCoord(counterPos[0])-1, calcCoord(counterPos[1])-1, 22, 22);
+			g.setColor(Color.RED);
+			counter(g, calcCoord(counterPos[0]-1), calcCoord(counterPos[1]+1));
+			counterPos[0] -= 1;
+			counterPos[1] += 1;
+		}
+		moveMho(g);
+	}
+
+	public void rightDown(Graphics g) {
+		if(isDeadPlayer(counterPos[0]+1, counterPos[1]+1)) {
+			dead(g);
+		}
+		else {
+			g.setColor(Color.BLACK);
+			g.fillRect(calcCoord(counterPos[0])-1, calcCoord(counterPos[1])-1, 22, 22);
+			g.setColor(Color.RED);
+			counter(g, calcCoord(counterPos[0]+1), calcCoord(counterPos[1]+1));
+			counterPos[0] += 1;
+			counterPos[1] += 1;
+		}
+		moveMho(g);
+	}
+
+	public void jump(Graphics g) {
+		c = rand.nextInt(11);
+		c += 2;
+		r = rand.nextInt(11);
+		r += 2;
+		if(isDeadPlayer(c, r)) {
+			dead(g);
+		}
+		else {
+			g.setColor(Color.BLACK);
+			g.fillRect(calcCoord(counterPos[0])-1, calcCoord(counterPos[1])-1, 22, 22);
+			g.setColor(Color.RED);
+			counter(g, calcCoord(c), calcCoord(r));
+			counterPos[0] = c;
+			counterPos[1] = r;
+		}
+		moveMho(g);
+	}
+
+	public void sit(Graphics g) {
+		moveMho(g);
+	}
+
 	// NOT FINISHED - key listener
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
-		if(key==KeyEvent.VK_A) {
-			System.out.println("Hello there");
+		if(key==KeyEvent.VK_W) {
 			var = 1;
+			repaint();
+		}
+		else if(key==KeyEvent.VK_X) {
+			var = 2;
+			repaint();
+		}
+		else if(key==KeyEvent.VK_Q) {
+			var = 3;
+			repaint();
+		}
+		else if(key==KeyEvent.VK_E) {
+			var = 4;
+			repaint();
+		}
+		else if(key==KeyEvent.VK_A) {
+			var = 5;
+			repaint();
+		}
+		else if(key==KeyEvent.VK_S) {
+			var = 6;
+			repaint();
+		}
+		else if(key==KeyEvent.VK_D) {
+			var = 7;
+			repaint();
+		}
+		else if(key==KeyEvent.VK_Z) {
+			var = 8;
+			repaint();
+		}
+		else if(key==KeyEvent.VK_C) {
+			var = 9;
+			repaint();
+		}
+		else if(key==KeyEvent.VK_J) {
+			var = 10;
 			repaint();
 		}
 	}
@@ -211,6 +415,7 @@ public class hiVolts extends JFrame implements KeyListener {
 
 	public hiVolts() {
 		addKeyListener(this);
+		addMouseListener(this);
 		init();
 	}
 
@@ -220,17 +425,73 @@ public class hiVolts extends JFrame implements KeyListener {
 		repaint();
 	}
 
+	public void mouseClicked(MouseEvent e) {
+		System.out.println("hi");
+
+	}
+
+	public void mousePressed(MouseEvent e) {
+
+
+	}
+
+	public void mouseReleased(MouseEvent e) {
+
+
+	}
+
+	public void mouseEntered(MouseEvent e) {
+
+
+	}
+
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+
 	// Paint
 	public void paint(Graphics g) {
-		
 		g.setColor(Color.YELLOW);
 		if(first==0) {
 			drawboard(g);
 		}
-		
+
 		if(var==1) {
-			moveMho(g);
+			up(g);
 		}
+		else if(var==2) {
+			down(g);
+		}
+		else if(var==3) {
+			leftUp(g);
+		}
+		else if(var==4) {
+			rightUp(g);
+		}
+		else if(var==5) {
+			left(g);
+		}
+		else if(var==6) {
+			sit(g);
+		}
+		else if(var==7) {
+			right(g);
+		}
+		else if(var==8) {
+			leftDown(g);
+		}
+		else if(var==9) {
+			rightDown(g);
+		}
+		else if(var==10) {
+			jump(g);
+		}
+
 		first = 1;
+
+		if(testHiVolts.end&&notPlay) {
+			first = 0;
+		}
 	}
 }
