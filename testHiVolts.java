69df201f7c1cb669297b5e6d00294a0e27a65109
Kamran <56656396+Kamran-B@users.noreply.github.com>
2019-11-02 10:05:15 -0700

    Update testHiVolts.java

--- testHiVolts.java
+++ testHiVolts.java
@@ -1,11 +1,16 @@
 
 public class testHiVolts {
 
+	public static boolean end;
+
 	public static void main(String[] args) {
+
 		hiVolts c = new hiVolts();
 		c.setDefaultCloseOperation(hiVolts.EXIT_ON_CLOSE);
 		c.setVisible(true);
 		c.setResizable(false);
-	}
+		if(end) {
 
+		}
+	}
 }
